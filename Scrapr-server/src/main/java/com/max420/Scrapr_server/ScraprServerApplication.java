f88460f5d7dd920b1b87fe0fdd703d66cec3120c
IamMax279 <maksssiula@gmail.com>
2025-09-24 21:45:31 +0200

    initial commit

---
+++ Scrapr-server/src/main/java/com/max420/Scrapr_server/ScraprServerApplication.java
@@ -0,0 +1,13 @@
+package com.max420.Scrapr_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScraprServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScraprServerApplication.class, args);
+	}
+
+}
---
+++ Scrapr-server/src/test/java/com/max420/Scrapr_server/ScraprServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max420.Scrapr_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraprServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
