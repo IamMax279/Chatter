479244bde351ce65996f9381d1e324b7761e126a
IamMax279 <maksssiula@gmail.com>
2025-09-28 20:09:21 +0200

    add component and entity scan to Application.java

--- Scrapr-server/src/main/java/com/max420/Scrapr_server/ScraprServerApplication.java
+++ Scrapr-server/src/main/java/com/max420/Scrapr_server/ScraprServerApplication.java
@@ -2,7 +2,11 @@ package com.max420.Scrapr_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.max420.Scrapr_server.models"})
+@EntityScan(basePackages = {"com.max420.Scrapr_server.models"})
 @SpringBootApplication
 public class ScraprServerApplication {
 
