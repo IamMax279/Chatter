f6bde9fefe8a0faa6a6f57445116899f45c2e186
IamMax279 <maksssiula@gmail.com>
2025-09-28 20:52:47 +0200

    add the config directory with SecurityConfig class and securityFilterChain() method

---
+++ Scrapr-server/src/main/java/com/max420/Scrapr_server/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.max420.Scrapr_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity https) throws Exception {
+        return https.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request.anyRequest().authenticated())
+                .httpBasic(customizer -> customizer.disable())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
