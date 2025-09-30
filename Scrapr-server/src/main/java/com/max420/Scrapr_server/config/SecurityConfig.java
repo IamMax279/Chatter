3b7fd889a7196bb6f67c1f409a24047ecfdff374
IamMax279 <maksssiula@gmail.com>
2025-09-30 21:17:55 +0200

    add PasswordEncoder bean to SecurityConfig

--- Scrapr-server/src/main/java/com/max420/Scrapr_server/config/SecurityConfig.java
+++ Scrapr-server/src/main/java/com/max420/Scrapr_server/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,10 +18,14 @@ public class SecurityConfig {
         return https.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request ->
                         request.anyRequest().authenticated())
-                .httpBasic(customizer -> customizer.disable())
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
