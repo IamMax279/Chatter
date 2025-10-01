22642bcf642d84b5994061c7e6f9b838725ed988
IamMax279 <maksssiula@gmail.com>
2025-10-01 21:10:27 +0200

    change the return type of createUser() from void to User

--- Scrapr-server/src/main/java/com/max420/Scrapr_server/services/UserService.java
+++ Scrapr-server/src/main/java/com/max420/Scrapr_server/services/UserService.java
@@ -1,7 +1,8 @@
 package com.max420.Scrapr_server.services;
 
 import com.max420.Scrapr_server.dto.user.UserRegistrationDto;
+import com.max420.Scrapr_server.models.User;
 
 public interface UserService {
-    void createUser(UserRegistrationDto userDto) throws IllegalArgumentException;
+    User createUser(UserRegistrationDto userDto) throws IllegalArgumentException;
 }
