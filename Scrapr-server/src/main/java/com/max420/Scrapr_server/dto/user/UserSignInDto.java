1ccf8d579694c17e95c89031a9a9599abe6a8a5f
IamMax279 <maksssiula@gmail.com>
2025-10-06 21:46:19 +0200

    add UserSignInDto

---
+++ Scrapr-server/src/main/java/com/max420/Scrapr_server/dto/user/UserSignInDto.java
@@ -0,0 +1,18 @@
+package com.max420.Scrapr_server.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UserSignInDto {
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Wrong email formtat.")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+}
