73ba40322fba3a17cea380eb0cea206e526115f4
IamMax279 <maksssiula@gmail.com>
2025-10-08 21:10:17 +0200

    forgor to include the interface

--- Scrapr-server/src/main/java/com/max420/Scrapr_server/services/impl/UserServiceImpl.java
+++ Scrapr-server/src/main/java/com/max420/Scrapr_server/services/impl/UserServiceImpl.java
@@ -1,20 +1,32 @@
 package com.max420.Scrapr_server.services.impl;
 
 import com.max420.Scrapr_server.dto.user.UserRegistrationDto;
+import com.max420.Scrapr_server.dto.user.UserSignInDto;
 import com.max420.Scrapr_server.models.User;
 import com.max420.Scrapr_server.repository.UserRepository;
+import com.max420.Scrapr_server.services.JwtService;
 import com.max420.Scrapr_server.services.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
     @Override
@@ -27,6 +39,15 @@ public class UserServiceImpl implements UserService {
         userRepository.save(user);
 
         return user;
+    }
+
+    @Override
+    public String signIn(UserSignInDto userDto) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(userDto.getEmail(), userDto.getPassword()));
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return jwtService.generateToken(userDetails.getUsername());
     }
 
     private User mapToUser(UserRegistrationDto userDto) {
