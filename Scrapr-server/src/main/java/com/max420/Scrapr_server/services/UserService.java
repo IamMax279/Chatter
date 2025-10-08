e283267c76bab81c6e2d977ad1365c5a5c3dcd29
IamMax279 <maksssiula@gmail.com>
2025-10-08 21:09:28 +0200

    finish the signIn() method in UserService

--- Scrapr-server/src/main/java/com/max420/Scrapr_server/services/UserService.java
+++ Scrapr-server/src/main/java/com/max420/Scrapr_server/services/UserService.java
@@ -1,8 +1,12 @@
 package com.max420.Scrapr_server.services;
 
 import com.max420.Scrapr_server.dto.user.UserRegistrationDto;
+import com.max420.Scrapr_server.dto.user.UserSignInDto;
 import com.max420.Scrapr_server.models.User;
+
+import javax.naming.AuthenticationException;
 
 public interface UserService {
     User createUser(UserRegistrationDto userDto) throws IllegalArgumentException;
+    String signIn(UserSignInDto userSignInDto);
 }
