89d2e86256ad3f653e26cfaaaf5369d8226b483e
IamMax279 <maksssiula@gmail.com>
2025-10-12 21:52:43 +0200

    delete Scrapr jsoup files, will go with selenium instead

--- Scrapr-server/src/main/java/com/max420/Scrapr_server/services/JsoupService.java
+++
@@ -1,8 +0,0 @@
-package com.max420.Scrapr_server.services;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface JsoupService {
-    List<String> fetchSomeData() throws IOException;
-}
