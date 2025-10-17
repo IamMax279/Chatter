31b9ae8c0d41be6549baa3cd728969d5729fecfd
IamMax279 <maksssiula@gmail.com>
2025-10-17 23:03:27 +0200

    decided to go with scraping crypto prices, twitter seems hostile towards selenium :/

--- Scrapr-server/src/main/java/com/max420/Scrapr_server/services/impl/SeleniumServiceImpl.java
+++ Scrapr-server/src/main/java/com/max420/Scrapr_server/services/impl/SeleniumServiceImpl.java
@@ -24,38 +24,20 @@ public class SeleniumServiceImpl implements SeleniumService {
     @Override
     public List<String> scrapeSomeData() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
+//      options.addArguments("--headless=new");
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://x.com/elonmusk");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://crypto.com/en/price");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // accept the cookies first
         WebElement cookieButton = wait.until(ExpectedConditions
-                .visibilityOfElementLocated(By.xpath("//span[contains(text(), 'Refuse non-essential cookies')]")));
+                .elementToBeClickable(By.xpath("//button[contains(text(), 'Reject non-essential cookies ')]")));
         cookieButton.click();
-        // click the sign-in button
-        WebElement signInButton = driver.findElement(By.xpath("//span[contains(text(), 'Zaloguj się')]"));
-        signInButton.click();
-        // log in using provided credentials
 
-        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='text']")));
-        emailField.sendKeys(twitterEmail);
-        WebElement proceed = driver.findElement(By.xpath("//span[contains(text(), 'Dalej')]"));
-        proceed.click();
-        // type in the password
-        WebElement passwordField = driver.findElement(By.className("r-30o5oe"));
-        passwordField.sendKeys(twitterPassword);
-        WebElement signIn = driver.findElement(By.className("css-1jxf684"));
-        signIn.click();
-
-        List<WebElement> titles = driver.findElements(By.className("css-1jxf684"));
-        List<String> deserialized = titles.stream()
-                .map(WebElement::getText)
-                .filter(text -> !text.isBlank())
-                .toList();
+        System.out.println("Przeglądarka pozostaje otwarta. Naciśnij Enter, aby zakończyć...");
+        new java.util.Scanner(System.in).nextLine();
 
         driver.quit();
-        return deserialized;
+        return List.of();
     }
 }
