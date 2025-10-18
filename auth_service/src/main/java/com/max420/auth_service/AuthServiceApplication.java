ed6ef8fca57b2e717deb277b8187f0a1e95f68f8
IamMax279 <maksssiula@gmail.com>
2025-10-18 21:39:38 +0200

    change - to _ in the service folder name

--- auth_service/src/main/java/com/max420/auth_service/AuthServiceApplication.java
+++ auth_service/src/main/java/com/max420/auth_service/AuthServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScraprServerApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScraprServerApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
