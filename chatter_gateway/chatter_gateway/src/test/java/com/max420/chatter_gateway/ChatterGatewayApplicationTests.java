e0877566ea5a10d65f45bef5968b6ab22b259277
IamMax279 <maksssiula@gmail.com>
2025-10-19 20:35:46 +0200

    add chatter_gateway

---
+++ chatter_gateway/chatter_gateway/src/test/java/com/max420/chatter_gateway/ChatterGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max420.chatter_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatterGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
