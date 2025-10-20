c12033dc02cdd23eabf7e2a24e2afa16f9f8eaa2
IamMax279 <maksssiula@gmail.com>
2025-10-20 22:22:24 +0200

    start implementing the gateway jwt filter

---
+++ chatter_gateway/src/main/java/com/max420/chatter_gateway/config/JwtAuthFilter.java
@@ -0,0 +1,35 @@
+package com.max420.chatter_gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+public class JwtAuthFilter implements GlobalFilter {
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    private static final List<String> PUBLIC_ENDPOINTS = List.of(
+            "/auth/sign-up",
+            "/auth/sign-in"
+    );
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getPath().toString();
+
+        if (PUBLIC_ENDPOINTS.stream().anyMatch(path::startsWith)) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            
+        }
+    }
+}
