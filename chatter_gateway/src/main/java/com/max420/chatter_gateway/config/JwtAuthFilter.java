e52bd3143d3bd9c4a95909df46180458bda208cf
IamMax279 <maksssiula@gmail.com>
2025-10-22 21:40:52 +0200

    finish the gateway JwtAuthFilter

--- chatter_gateway/src/main/java/com/max420/chatter_gateway/config/JwtAuthFilter.java
+++ chatter_gateway/src/main/java/com/max420/chatter_gateway/config/JwtAuthFilter.java
@@ -1,5 +1,7 @@
 package com.max420.chatter_gateway.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -7,7 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Component
@@ -24,14 +29,36 @@ public class JwtAuthFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String path = exchange.getRequest().getPath().toString();
 
+        // if the path is a public endpoint, proceed to the next filter
         if (PUBLIC_ENDPOINTS.stream().anyMatch(path::startsWith)) {
             return chain.filter(exchange);
         }
 
         String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+        // if there is no jwt token, terminate the flow
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
+
+        try {
+            String token = authHeader.substring(7);
+            SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            exchange = exchange.mutate()
+                    .request(r -> r.headers(h -> h.add("X-User-email", claims.getSubject())))
+                    .build();
+        } catch (Exception e) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
     }
 }
