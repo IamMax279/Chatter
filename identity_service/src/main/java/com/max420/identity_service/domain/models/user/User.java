9a61225b7b613edbb59b9c68cc9d3cebf6726508
IamMax279 <maksssiula@gmail.com>
2025-11-03 22:08:28 +0100

    start implementing the domain layer (moved away from N-tier to DDD)

---
+++ identity_service/src/main/java/com/max420/identity_service/domain/models/user/User.java
@@ -0,0 +1,7 @@
+package com.max420.identity_service.domain.models.user;
+
+public class User {
+    private UserId id;
+
+    // TODO: przemyslec jakie pola ma miec User i zaimplementowac VO dla odpowiednich pól
+}
---
+++ identity_service/src/main/java/com/max420/identity_service/domain/models/user/UserId.java
@@ -0,0 +1,9 @@
+package com.max420.identity_service.domain.models.user;
+
+public class UserId {
+    private Long value;
+
+    public Long getValue() {
+        return value;
+    }
+}
