77d85cf3758f0accf36b77b8aa866a30851885f3
IamMax279 <maksssiula@gmail.com>
2025-11-08 19:14:05 +0100

    finish the User aggregate

--- identity_service/src/main/java/com/max420/identity_service/domain/models/user/User.java
+++ identity_service/src/main/java/com/max420/identity_service/domain/models/user/User.java
@@ -1,15 +1,12 @@
 package com.max420.identity_service.domain.models.user;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class User {
@@ -19,6 +16,7 @@ public class User {
     private Username username;
     private Set<Role> roles;
     private boolean isActive;
+    private boolean isVerified;
     private final Instant createdAt;
 
     public User(UserId id, Email email, Username username) {
@@ -28,11 +26,20 @@ public class User {
         this.roles = new HashSet<>();
         this.roles.add(Role.USER);
         this.isActive = true;
+        this.isVerified = false;
         this.createdAt = Instant.now();
     }
 
     public void addRole(Role role) {
         this.roles.add(role);
+    }
+
+    public void removeRole(Role role) {
+        this.roles.remove(role);
+    }
+
+    public boolean hasRole(Role role) {
+        return this.roles.contains(role);
     }
 
     public void changePassword(Password oldPassword, Password newPassword) {
@@ -43,8 +50,12 @@ public class User {
         this.password = newPassword;
     }
 
+    public boolean verifyPassword(Password passwordCandidate) {
+        return this.password.value().matches(passwordCandidate.value());
+    }
+
     public void changeUsername(Username username) {
-        if (username.toString().isBlank() || username.toString().length() < 3) {
+        if (username.value().isBlank() || username.value().length() < 3) {
             throw new IllegalArgumentException("Username must be at least 3 characters long.");
         }
         if (this.username == username) {
@@ -52,5 +63,17 @@ public class User {
         }
 
         this.username = username;
+    }
+
+    public void activate() {
+        this.isActive = true;
+    }
+
+    public void deactivate() {
+        this.isActive = false;
+    }
+
+    public void verify() {
+        this.isVerified = true;
     }
 }
