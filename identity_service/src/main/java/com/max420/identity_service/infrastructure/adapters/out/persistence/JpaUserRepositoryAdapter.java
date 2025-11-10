17041866c7199eafa6d296a2c5613b0db641863b
IamMax279 <maksssiula@gmail.com>
2025-11-10 20:24:16 +0100

    forgot to add the @Repository annotation

--- identity_service/src/main/java/com/max420/identity_service/infrastructure/adapters/out/persistence/JpaUserRepositoryAdapter.java
+++ identity_service/src/main/java/com/max420/identity_service/infrastructure/adapters/out/persistence/JpaUserRepositoryAdapter.java
@@ -2,6 +2,8 @@ package com.max420.identity_service.infrastructure.adapters.out.persistence;
 
 import com.max420.identity_service.application.ports.out.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaUserRepositoryAdapter extends UserRepository, JpaRepository<UserEntity, Long>{
 }
