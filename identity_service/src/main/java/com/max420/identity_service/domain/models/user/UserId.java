e46f653740e2bad493b2e163dc077557624aad3f
IamMax279 <maksssiula@gmail.com>
2025-11-10 19:15:32 +0100

    add .newId() method to UserId VO

--- identity_service/src/main/java/com/max420/identity_service/domain/models/user/UserId.java
+++ identity_service/src/main/java/com/max420/identity_service/domain/models/user/UserId.java
@@ -1,3 +1,9 @@
 package com.max420.identity_service.domain.models.user;
 
-public record UserId(Long value) {}
+import java.util.UUID;
+
+public record UserId(String value) {
+    public static UserId newId() {
+        return new UserId(UUID.randomUUID().toString());
+    }
+}
