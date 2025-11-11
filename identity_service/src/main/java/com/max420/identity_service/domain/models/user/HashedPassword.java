976089bc92fba26c53842d2552eb9f33b3045ad9
IamMax279 <maksssiula@gmail.com>
2025-11-11 19:14:27 +0100

    update VOs

--- identity_service/src/main/java/com/max420/identity_service/domain/models/user/HashedPassword.java
+++ identity_service/src/main/java/com/max420/identity_service/domain/models/user/HashedPassword.java
@@ -1,4 +1,10 @@
 package com.max420.identity_service.domain.models.user;
 
+import java.util.Objects;
+
 public record HashedPassword(String value) {
+    public HashedPassword(String value) {
+        Objects.requireNonNull(value);
+        this.value = value;
+    }
 }
--- identity_service/src/main/java/com/max420/identity_service/domain/models/user/UserId.java
+++ identity_service/src/main/java/com/max420/identity_service/domain/models/user/UserId.java
@@ -2,8 +2,8 @@ package com.max420.identity_service.domain.models.user;
 
 import java.util.UUID;
 
-public record UserId(String value) {
+public record UserId(UUID value) {
     public static UserId newId() {
-        return new UserId(UUID.randomUUID().toString());
+        return new UserId(UUID.randomUUID());
     }
 }
