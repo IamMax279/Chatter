0d059a7c2d345003214b17cd51ed95bf08103f29
IamMax279 <maksssiula@gmail.com>
2025-11-11 19:22:08 +0100

    add inbound adapters directory

---
+++ identity_service/src/main/java/com/max420/identity_service/infrastructure/adapters/in/controllers/UserController.java
@@ -0,0 +1,8 @@
+package com.max420.identity_service.infrastructure.adapters.in.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
