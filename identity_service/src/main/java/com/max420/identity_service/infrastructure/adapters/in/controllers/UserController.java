fa44eee1f4b60d9acc60e4e4b8ee31af7c3fd0d0
IamMax279 <maksssiula@gmail.com>
2025-11-12 11:42:16 +0100

    add basic implementation of register user endpoint

--- identity_service/src/main/java/com/max420/identity_service/infrastructure/adapters/in/controllers/UserController.java
+++ identity_service/src/main/java/com/max420/identity_service/infrastructure/adapters/in/controllers/UserController.java
@@ -1,8 +1,25 @@
 package com.max420.identity_service.infrastructure.adapters.in.controllers;
 
+import com.max420.identity_service.application.commands.RegisterUserCommand;
+import com.max420.identity_service.application.ports.in.RegisterUserUseCase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class UserController {
+    private final RegisterUserUseCase registerUserUseCase;
 
+    public UserController(RegisterUserUseCase registerUserUseCase) {
+        this.registerUserUseCase = registerUserUseCase;
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<?> signUp(@RequestBody RegisterUserCommand command) {
+        registerUserUseCase.execute(command);
+        return ResponseEntity.ok("success");
+    }
 }
