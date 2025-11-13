86e85144d811c17cf810046e4b88c6e1d4917c3d
IamMax279 <maksssiula@gmail.com>
2025-11-13 22:46:35 +0100

    im in the middle of updating the Email VO cause Json is screaming it can't parse the VO

--- chatter/src/main/java/com/max420/identity_service/domain/models/user/Email.java
+++ chatter/src/main/java/com/max420/identity_service/domain/models/user/Email.java
@@ -1,10 +1,13 @@
 package com.max420.identity_service.domain.models.user;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.max420.identity_service.domain.exceptions.InvalidEmailException;
 
 import java.util.regex.Pattern;
 
 public record Email(String value) {
+
+    @JsonCreator
     public Email(String value) {
         Pattern pattern = Pattern.compile("\\b[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}\\b");
         if (!pattern.matcher(value).matches()) {
