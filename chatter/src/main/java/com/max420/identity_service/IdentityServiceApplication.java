bc59c4d94bd9d868b2eedf92703a323455f1d72a
IamMax279 <maksssiula@gmail.com>
2025-11-13 22:45:53 +0100

    temporarily disable spring security

--- chatter/src/main/java/com/max420/identity_service/IdentityServiceApplication.java
+++ chatter/src/main/java/com/max420/identity_service/IdentityServiceApplication.java
@@ -2,9 +2,10 @@ package com.max420.identity_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
