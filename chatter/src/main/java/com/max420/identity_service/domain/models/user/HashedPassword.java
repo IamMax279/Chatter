408bf9cea18cda4a9f9cf39761580a765118e7a1
IamMax279 <maksssiula@gmail.com>
2025-11-14 21:35:24 +0100

    add @JsonCreator to other VOs/improve validation

--- chatter/src/main/java/com/max420/identity_service/domain/models/user/HashedPassword.java
+++ chatter/src/main/java/com/max420/identity_service/domain/models/user/HashedPassword.java
@@ -3,8 +3,9 @@ package com.max420.identity_service.domain.models.user;
 import java.util.Objects;
 
 public record HashedPassword(String value) {
-    public HashedPassword(String value) {
-        Objects.requireNonNull(value);
-        this.value = value;
+    public HashedPassword {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("Invalid hashed password.");
+        }
     }
 }
