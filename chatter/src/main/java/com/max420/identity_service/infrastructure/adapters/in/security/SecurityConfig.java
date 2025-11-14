179c1d2bbe93b3328882105932ee1e5cbdbe2f66
IamMax279 <maksssiula@gmail.com>
2025-11-14 16:44:12 +0100

    add custom security filter chain impl

---
+++ chatter/src/main/java/com/max420/identity_service/infrastructure/adapters/in/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.max420.identity_service.infrastructure.adapters.in.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/auth/sign-up", "/auth/sign-in")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
