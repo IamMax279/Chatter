28a0e85c86da51e95a865281c7088de55054d009
IamMax279 <maksssiula@gmail.com>
2025-11-14 16:43:37 +0100

    disable default spring security settings

--- chatter/src/main/java/com/max420/identity_service/IdentityServiceApplication.java
+++ chatter/src/main/java/com/max420/identity_service/IdentityServiceApplication.java
@@ -3,9 +3,13 @@ package com.max420.identity_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class
+})
 public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
