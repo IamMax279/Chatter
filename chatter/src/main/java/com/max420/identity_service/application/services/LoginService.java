bf90f8df8390ee7c0ca725e8fd0404b4c7da9b66
IamMax279 <maksssiula@gmail.com>
2025-11-15 22:01:37 +0100

    rename imported modules accordingly

--- chatter/src/main/java/com/max420/identity_service/application/services/LoginService.java
+++ chatter/src/main/java/com/max420/identity_service/application/services/LoginService.java
@@ -2,17 +2,29 @@ package com.max420.identity_service.application.services;
 
 import com.max420.identity_service.application.commands.LoginCommand;
 import com.max420.identity_service.application.ports.in.LoginUseCase;
-import com.max420.identity_service.application.ports.out.JwtTokenGenerator;
+import com.max420.identity_service.application.ports.out.JwtService;
 import com.max420.identity_service.application.ports.out.PasswordHasher;
 import com.max420.identity_service.application.ports.out.UserRepository;
 import com.max420.identity_service.domain.exceptions.InvalidPasswordException;
 import com.max420.identity_service.domain.exceptions.UserNotFoundException;
 import com.max420.identity_service.domain.models.user.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoginService implements LoginUseCase {
-    private UserRepository userRepository;
-    private JwtTokenGenerator generator;
-    private PasswordHasher passwordHasher;
+    private final UserRepository userRepository;
+    private final JwtService generator;
+    private final PasswordHasher passwordHasher;
+
+    public LoginService(
+            UserRepository userRepository,
+            JwtService generator,
+            PasswordHasher passwordHasher
+    ) {
+        this.userRepository = userRepository;
+        this.generator = generator;
+        this.passwordHasher = passwordHasher;
+    }
 
     @Override
     public String execute(LoginCommand command) {
@@ -22,7 +34,7 @@ public class LoginService implements LoginUseCase {
 
         User user = userRepository.findByEmail(command.email()).get();
         if (passwordHasher.compare(user.getPassword(), command.password())) {
-            return generator.generateToken();
+            return generator.generateToken(); // TODO: zaimplementowac adapter jwt
         } else {
             throw new InvalidPasswordException("Passwords don't match");
         }
