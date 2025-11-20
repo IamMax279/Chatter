e6919808c0a480c5855a403664a1a7f59c92528a
IamMax279 <maksssiula@gmail.com>
2025-11-20 21:53:34 +0100

    update UserController

--- chatter/src/main/java/com/max420/identity_service/infrastructure/adapters/in/controllers/UserController.java
+++ chatter/src/main/java/com/max420/identity_service/infrastructure/adapters/in/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.max420.identity_service.infrastructure.adapters.in.controllers;
 
+import com.max420.identity_service.application.commands.ChangeBioCommand;
 import com.max420.identity_service.application.commands.ChangeUsernameCommand;
+import com.max420.identity_service.application.ports.in.ChangeBioUseCase;
 import com.max420.identity_service.application.ports.in.ChangeUsernameUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,14 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
     private final ChangeUsernameUseCase changeUsernameUseCase;
+    private final ChangeBioUseCase changeBioUseCase;
 
-    public UserController(ChangeUsernameUseCase changeUsernameUseCase) {
+    public UserController(
+            ChangeUsernameUseCase changeUsernameUseCase,
+            ChangeBioUseCase changeBioUseCase
+    ) {
         this.changeUsernameUseCase = changeUsernameUseCase;
+        this.changeBioUseCase = changeBioUseCase;
     }
 
     @PostMapping("/change-username")
     public ResponseEntity<?> changeUsername(@RequestBody ChangeUsernameCommand command) {
         changeUsernameUseCase.execute(command);
         return ResponseEntity.ok("Your username has been changed successfully");
+    }
+
+    @PostMapping("/change-bio")
+    public ResponseEntity<?> changeBio(@RequestBody ChangeBioCommand command) {
+        changeBioUseCase.execute(command);
+        return ResponseEntity.ok("Your bio has been changed successfully");
     }
 }
