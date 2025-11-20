7d37c65f2f411ca9a04da014b4640b05c7cd0453
IamMax279 <maksssiula@gmail.com>
2025-11-20 21:53:07 +0100

    add ChangeUsername logic

--- chatter/src/main/java/com/max420/identity_service/application/commands/ChangeUsernameCommand.java
+++ chatter/src/main/java/com/max420/identity_service/application/commands/ChangeUsernameCommand.java
@@ -6,11 +6,9 @@ import com.max420.identity_service.domain.models.user.Username;
 import java.util.Objects;
 
 public record ChangeUsernameCommand(
-        Username newUsername,
-        Email email
+        Username newUsername
 ) {
     public ChangeUsernameCommand {
         Objects.requireNonNull(newUsername);
-        Objects.requireNonNull(email);
     }
 }
--- chatter/src/main/java/com/max420/identity_service/application/services/ChangeUsernameService.java
+++ chatter/src/main/java/com/max420/identity_service/application/services/ChangeUsernameService.java
@@ -4,25 +4,37 @@ import com.max420.identity_service.application.commands.ChangeUsernameCommand;
 import com.max420.identity_service.application.ports.in.ChangeUsernameUseCase;
 import com.max420.identity_service.application.ports.out.UserRepository;
 import com.max420.identity_service.domain.exceptions.UserNotFoundException;
+import com.max420.identity_service.domain.models.user.Email;
 import com.max420.identity_service.domain.models.user.User;
+import com.max420.identity_service.infrastructure.adapters.out.persistence.UserEntity;
+import com.max420.identity_service.infrastructure.adapters.out.persistence.UserEntityMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class ChangeUsernameService implements ChangeUsernameUseCase {
     private final UserRepository userRepository;
+    private final UserEntityMapper mapper;
 
-    public ChangeUsernameService(UserRepository userRepository) {
+    public ChangeUsernameService(UserRepository userRepository, UserEntityMapper mapper) {
         this.userRepository = userRepository;
+        this.mapper = mapper;
     }
 
     @Override
     public void execute(ChangeUsernameCommand command) {
-        User user = userRepository.findByEmail(command.email())
-                .orElseThrow(() -> new UserNotFoundException("User with such email address does not exist"));
+        Object email = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
+        if (!(email instanceof String)) {
+            throw new IllegalStateException("The auth principal is not a string");
+        }
+
+        UserEntity entity = userRepository.findByEmail(new Email((String) email))
+                .orElseThrow(() -> new UserNotFoundException("User with such email address does not exist"));
+        User user = mapper.toUser(entity);
         user.changeUsername(command.newUsername());
-        userRepository.save(user);
+
+        mapper.updateEntity(entity, user);
+        userRepository.update(entity);
     }
 }
