3735114e6d24cb704e4d8708c16017c01dd945eb
IamMax279 <maksssiula@gmail.com>
2025-11-28 20:24:08 +0100

    add application services that implement the recently added ports

---
+++ chatter/src/main/java/com/max420/chatter/application/services/AuthService.java
@@ -0,0 +1,64 @@
+package com.max420.chatter.application.services;
+
+import com.max420.chatter.application.commands.auth.LoginCommand;
+import com.max420.chatter.application.commands.auth.RegisterUserCommand;
+import com.max420.chatter.application.ports.in.AuthPort;
+import com.max420.chatter.application.ports.out.JwtService;
+import com.max420.chatter.application.ports.out.PasswordHasher;
+import com.max420.chatter.application.ports.out.UserRepository;
+import com.max420.chatter.domain.exceptions.user.EmailTakenException;
+import com.max420.chatter.domain.exceptions.user.InvalidPasswordException;
+import com.max420.chatter.domain.exceptions.user.UserNotFoundException;
+import com.max420.chatter.domain.models.user.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService implements AuthPort {
+    private final UserRepository userRepository;
+    private final PasswordHasher passwordHasher;
+    private final JwtService jwtService;
+
+    public AuthService(
+            UserRepository userRepository,
+            PasswordHasher passwordHasher,
+            JwtService jwtService
+    ) {
+        this.userRepository = userRepository;
+        this.passwordHasher = passwordHasher;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public UserId register(RegisterUserCommand command) {
+        if (userRepository.existsByEmail(command.email())) {
+            throw new EmailTakenException("Account with this email already exists");
+        }
+
+        HashedPassword hashedPassword = passwordHasher.hash(command.password());
+
+        UserId id = UserId.newId();
+        Email email = command.email();
+        Username username = command.username();
+
+        User user = new User(id, email, username, hashedPassword);
+        userRepository.save(user);
+
+        return id;
+    }
+
+    @Override
+    public String login(LoginCommand command) {
+        Optional<User> user = userRepository.findByEmailAndMap(command.email());
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("Account with this email doesn't exist");
+        }
+
+        if (passwordHasher.compare(user.get().getPassword(), command.password())) {
+            return jwtService.generateToken(command.email().value(), user.get().getRoles());
+        } else {
+            throw new InvalidPasswordException("Passwords don't match");
+        }
+    }
+}
---
+++ chatter/src/main/java/com/max420/chatter/application/services/UserService.java
@@ -0,0 +1,58 @@
+package com.max420.chatter.application.services;
+
+import com.max420.chatter.application.commands.user.ChangeBioCommand;
+import com.max420.chatter.application.commands.user.ChangeUsernameCommand;
+import com.max420.chatter.application.ports.in.UserPort;
+import com.max420.chatter.application.ports.out.UserRepository;
+import com.max420.chatter.domain.exceptions.user.UserNotFoundException;
+import com.max420.chatter.domain.models.user.Email;
+import com.max420.chatter.domain.models.user.User;
+import com.max420.chatter.infrastructure.adapters.out.persistence.user.UserEntity;
+import com.max420.chatter.infrastructure.adapters.out.persistence.user.UserEntityMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserPort {
+    private final UserRepository userRepository;
+    private final UserEntityMapper mapper;
+
+    public UserService(UserRepository userRepository, UserEntityMapper mapper) {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void changeUsername(ChangeUsernameCommand command) {
+        Object email = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (!(email instanceof String)) {
+            throw new IllegalStateException("The auth principal is not a string");
+        }
+
+        UserEntity entity = userRepository.findByEmail(new Email((String) email))
+                .orElseThrow(() -> new UserNotFoundException("User with such email address does not exist"));
+        User user = mapper.toUser(entity);
+        user.changeUsername(command.newUsername());
+
+        mapper.updateEntity(entity, user);
+        userRepository.update(entity);
+    }
+
+    @Override
+    public void changeBio(ChangeBioCommand command) {
+        Object email = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (!(email instanceof String)) {
+            throw new IllegalStateException("The auth principal is not a string");
+        }
+
+        UserEntity entity = userRepository.findByEmail(new Email((String) email))
+                .orElseThrow(() -> new UserNotFoundException("User with such email address does not exist"));
+        User user = mapper.toUser(entity);
+        user.changeBio(command.bio());
+
+        mapper.updateEntity(entity, user);
+        userRepository.update(entity);
+    }
+}
