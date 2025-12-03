eedfbd9dc53f3b9514e8477e6d7f4c5bda0c474a
IamMax279 <maksssiula@gmail.com>
2025-12-03 12:29:07 +0100

    change jwt service port signature

--- chatter/src/main/java/com/max420/chatter/application/ports/out/JwtService.java
+++ chatter/src/main/java/com/max420/chatter/application/ports/out/JwtService.java
@@ -5,7 +5,8 @@ import com.max420.chatter.domain.models.user.Role;
 import java.util.Set;
 
 public interface JwtService {
-    String generateToken(String email, Set<Role> roles);
+    String generateToken(String userId, String email, Set<Role> roles);
+    Long extractUserId(String token);
     String extractEmail(String token);
     Set<Role> extractRoles(String token);
     boolean validateToken(String token);
