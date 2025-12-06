7b72bbd40837414a11822518788d9b93cb91df1a
IamMax279 <maksssiula@gmail.com>
2025-12-06 21:32:12 +0100

    init post repo

--- chatter/src/main/java/com/max420/chatter/application/ports/out/PostRepository.java
+++ chatter/src/main/java/com/max420/chatter/application/ports/out/PostRepository.java
@@ -1,4 +1,11 @@
 package com.max420.chatter.application.ports.out;
 
+import com.max420.chatter.domain.models.post.Post;
+import com.max420.chatter.domain.models.post.PostId;
+
+import java.util.Optional;
+
 public interface PostRepository {
+    void save(Post post);
+    Optional<Post> findById(PostId id);
 }
