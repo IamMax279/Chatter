b807427e110c6989f5e7a1eb936e765cc3354537
IamMax279 <maksssiula@gmail.com>
2025-12-06 21:32:29 +0100

    implement createPost() method

--- chatter/src/main/java/com/max420/chatter/application/services/PostService.java
+++ chatter/src/main/java/com/max420/chatter/application/services/PostService.java
@@ -2,18 +2,38 @@ package com.max420.chatter.application.services;
 
 import com.max420.chatter.application.commands.post.CreatePostCommand;
 import com.max420.chatter.application.ports.in.PostPort;
+import com.max420.chatter.application.ports.out.PostRepository;
 import com.max420.chatter.domain.models.post.Content;
+import com.max420.chatter.domain.models.post.Post;
 import com.max420.chatter.domain.models.post.PostId;
 import com.max420.chatter.domain.models.post.Title;
+import com.max420.chatter.domain.models.user.UserId;
+import com.max420.chatter.infrastructure.adapters.in.dto.AuthPrincipalDto;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PostService implements PostPort {
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 
     @Override
     public PostId createPost(CreatePostCommand command) {
+        Object context = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (!(context instanceof AuthPrincipalDto)) {
+            throw new IllegalStateException("The auth principal object is invalid");
+        }
+
         PostId id = PostId.newPostId();
         Title title = command.title();
         Content content = command.content();
+        UserId authorId = new UserId(((AuthPrincipalDto) context).userId());
 
-
+        Post post = new Post(id, title, content, authorId);
+        postRepository.save(post);
+        return id;
     }
 }
