698194cad7ec375954e188e96859084391f12fc4
IamMax279 <maksssiula@gmail.com>
2025-12-06 21:33:23 +0100

    add canonical constructor to UserId

--- chatter/src/main/java/com/max420/chatter/domain/models/user/UserId.java
+++ chatter/src/main/java/com/max420/chatter/domain/models/user/UserId.java
@@ -1,8 +1,13 @@
 package com.max420.chatter.domain.models.user;
 
+import java.util.Objects;
 import java.util.Random;
 
 public record UserId(Long value) {
+    public UserId {
+        Objects.requireNonNull(value);
+    }
+
     public static UserId newId() {
         return new UserId(new Random().nextLong());
     }
