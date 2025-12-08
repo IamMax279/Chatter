64ac389a18955553f96e135721a533ea95cd594b
IamMax279 <maksssiula@gmail.com>
2025-12-08 22:21:38 +0100

    idek

--- chatter/src/main/java/com/max420/chatter/infrastructure/persistence/post/JpaPostRepository.java
+++ chatter/src/main/java/com/max420/chatter/infrastructure/persistence/post/JpaPostRepository.java
@@ -1,6 +1,8 @@
 package com.max420.chatter.infrastructure.persistence.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaPostRepository extends JpaRepository<PostEntity, Long> {
 }
