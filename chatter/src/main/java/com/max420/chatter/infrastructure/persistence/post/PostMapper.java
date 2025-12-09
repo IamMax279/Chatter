98d3cd491ee03e77fa2f81a0ee3d516a4a1c4dca
IamMax279 <maksssiula@gmail.com>
2025-12-09 20:20:19 +0100

    finish the toPost and toPostEntity methods

--- chatter/src/main/java/com/max420/chatter/infrastructure/persistence/post/PostMapper.java
+++ chatter/src/main/java/com/max420/chatter/infrastructure/persistence/post/PostMapper.java
@@ -1,6 +1,10 @@
 package com.max420.chatter.infrastructure.persistence.post;
 
+import com.max420.chatter.domain.models.post.Content;
 import com.max420.chatter.domain.models.post.Post;
+import com.max420.chatter.domain.models.post.PostId;
+import com.max420.chatter.domain.models.post.Title;
+import com.max420.chatter.domain.models.user.UserId;
 import com.max420.chatter.infrastructure.persistence.user.UserEntity;
 import org.springframework.stereotype.Component;
 
@@ -12,5 +16,14 @@ public class PostMapper {
                 .content(post.getContent().value())
                 .author(author)
                 .build();
+    }
+
+    public Post toPost(PostEntity entity) {
+        return new Post(
+                new PostId(entity.getId()),
+                new Title(entity.getTitle()),
+                new Content(entity.getContent()),
+                new UserId(entity.getAuthor().getId())
+        );
     }
 }
