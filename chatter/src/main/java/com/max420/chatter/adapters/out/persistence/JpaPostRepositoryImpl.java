270b66f4544ab3184684d336b2de9ad5a23adf53
IamMax279 <maksssiula@gmail.com>
2025-12-09 20:21:00 +0100

    implement post repo class

--- chatter/src/main/java/com/max420/chatter/adapters/out/persistence/JpaPostRepositoryImpl.java
+++ chatter/src/main/java/com/max420/chatter/adapters/out/persistence/JpaPostRepositoryImpl.java
@@ -1,26 +1,41 @@
 package com.max420.chatter.adapters.out.persistence;
 
 import com.max420.chatter.application.ports.out.PostRepository;
+import com.max420.chatter.domain.exceptions.post.AuthorNotFoundException;
 import com.max420.chatter.domain.models.post.Post;
 import com.max420.chatter.domain.models.post.PostId;
 import com.max420.chatter.infrastructure.persistence.post.JpaPostRepository;
+import com.max420.chatter.infrastructure.persistence.post.PostMapper;
+import com.max420.chatter.infrastructure.persistence.user.JpaUserRepository;
+import com.max420.chatter.infrastructure.persistence.user.UserEntity;
 
 import java.util.Optional;
 
 public class JpaPostRepositoryImpl implements PostRepository {
-    private final JpaPostRepository jpaPostRepository;
+    private final JpaPostRepository postRepository;
+    private final JpaUserRepository userRepository;
+    private final PostMapper mapper;
 
-    public JpaPostRepositoryImpl(JpaPostRepository jpaPostRepository) {
-        this.jpaPostRepository = jpaPostRepository;
+    public JpaPostRepositoryImpl(
+            JpaPostRepository postRepository,
+            JpaUserRepository userRepository,
+            PostMapper mapper
+    ) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.mapper = mapper;
     }
 
     @Override
     public void save(Post post) {
+        UserEntity entity = userRepository.findById(post.getAuthorId().value())
+                .orElseThrow(() -> new AuthorNotFoundException("Post author not found"));
 
+        postRepository.save(mapper.toPostEntity(post, entity));
     }
 
     @Override
     public Optional<Post> findById(PostId id) {
-        return Optional.empty();
+        return postRepository.findById(id.value()).map(mapper::toPost);
     }
 }
