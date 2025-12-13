dac2751b4409fa147b515e4ba47d0abf1c4c871a
IamMax279 <maksssiula@gmail.com>
2025-12-13 23:18:14 +0100

    update auth controller

--- chatter/src/main/java/com/max420/chatter/adapters/in/controllers/AuthController.java
+++ chatter/src/main/java/com/max420/chatter/adapters/in/controllers/AuthController.java
@@ -1,8 +1,13 @@
 package com.max420.chatter.adapters.in.controllers;
 
+import com.max420.chatter.adapters.in.dto.user.RegisterUserRequest;
+import com.max420.chatter.adapters.in.dto.user.SignInRequest;
 import com.max420.chatter.application.commands.auth.LoginCommand;
 import com.max420.chatter.application.commands.auth.RegisterUserCommand;
 import com.max420.chatter.application.ports.in.AuthPort;
+import com.max420.chatter.domain.models.user.Email;
+import com.max420.chatter.domain.models.user.Password;
+import com.max420.chatter.domain.models.user.Username;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,14 +24,21 @@ public class AuthController {
     }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<?> signUp(@RequestBody RegisterUserCommand command) {
-        authPort.register(command);
+    public ResponseEntity<?> signUp(@RequestBody RegisterUserRequest request) {
+        authPort.register(new RegisterUserCommand(
+                new Email(request.email()),
+                new Password(request.password()),
+                new Username(request.username())
+        ));
         return ResponseEntity.ok("success");
     }
 
     @PostMapping("/sign-in")
-    public ResponseEntity<?> signIn(@RequestBody LoginCommand command) {
-        String token = authPort.login(command);
+    public ResponseEntity<?> signIn(@RequestBody SignInRequest request) {
+        String token = authPort.login(new LoginCommand(
+                new Email(request.email()),
+                new Password(request.password())
+        ));
         return ResponseEntity.ok(token);
     }
 }
